85ad8b0c37e74c91da21596595af9f3b7d8098a5
Younes Anys <yanys048@uottawa.ca>
2021-03-31 16:23:12 -0400

    my commit

---
+++ database/4_delete_data.sql
@@ -0,0 +1,10 @@
+
+DELETE FROM athletes WHERE id=1000000005;
+
+
+DELETE FROM athletes WHERE athlName="Matthew Thomas";
+
+DELETE FROM results WHERE comp_name="The uOttawa Tournament";
+DELETE FROM events WHERE comp_name="The uOttawa Tournament";
+DELETE FROM registrations WHERE comp_name="The uOttawa Tournament";
+DELETE FROM competitions WHERE comp_name="The uOttawa Tournament";
---
+++ database/5_update_data.sql
@@ -0,0 +1,14 @@
+
+UPDATE athletes
+SET athlName="Josh Lee"
+WHERE athlName="Josh Baker";
+
+
+UPDATE athletes
+SET DOB="2002-03-06"
+WHERE id=1000000007;
+
+
+UPDATE competitions
+SET starttime="12:45:00"
+WHERE com_name="Bob Smith Competition";
